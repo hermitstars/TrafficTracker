5fa2a81cbbf065a7442912627c7f8d99202ec1e4
xin9fan <starshermit@gmail.com>
2016-06-09 02:13:21 -0400

    update app.py

--- app.py
+++ app.py
@@ -4,12 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    color1 = '#F14722'
-    color2 = '#E67E22'
-    color3 = '#F4D03F'
-    color4 = '#27AE60'
-    colors = [color1, color2, color3, color4]
-    return render_template('index.html', colors = colors)
+    colors = ['#F14722', '#E67E22', '#F4D03F', '#27AE60']
+    n_car = [100, 90, 80, 70]
+    return render_template('index.html', colors=colors, n_car=n_car)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
